634ad511861c92029905d36cdc8d00302a5364fc
NotYourFridge <romanopoeran@gmail.com>
2023-10-14 01:49:57 +0200

    paar classes gemaakt

--- Opdracht_wek_4.3.cs
+++ Opdracht_wek_4.3.cs
@@ -1,13 +1,71 @@
-namespace verhuur{
+using System.ComponentModel.DataAnnotations;
 
-class Verhuuder{
+namespace verhuur
+{
 
-string naam{get;set;}
+    class Gebruiker
+    {
 
-}
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string naam { get; set; }
+
+        [Required]
+        public int telefoonnummer { get; set; }
+
+        public virtual Adres adres { get; set; }
+
+    }
+
+
+    class Verhuuder : Gebruiker
+    {
+
+        [Required]
+        public override Adres adres { get; set; }
+
+        public List<Auto> te_verhuren_autos{get;set;}
+    }
+
+    class Huurder : Gebruiker
+    {
+
+    }
+
+    class Adres
+    {
+
+        public string straat { get; set; }
+
+        public string plaats { get; set; }
+
+        public string postcode { get; set; }
+
+    }
+
+    class Auto
+    {
+
+        [Required]
+        int id { get; set; }
+
+        [Required]
+        string merk { get; set; }
+
+    }
+
+
+    class Reservering{
+
+
+
+        
+    }
 
 
 
 
-    
+
 }
